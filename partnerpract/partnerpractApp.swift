e766c6378dea264965d13e2ac65970c762bca62a
penelope <penelope.elgort@gmail.com>
2023-07-14 14:20:52 -0400

    Initial Commit

---
+++ partnerpract/partnerpractApp.swift
@@ -0,0 +1,17 @@
+//
+//  partnerpractApp.swift
+//  partnerpract
+//
+//  Created by Mia Elgort on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct partnerpractApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
